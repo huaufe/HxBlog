3f23873e319f135746b0f4ece96fd9866a85b7d7
胡兴 <huxing_aufe@163.com>
2023-02-01 11:22:12 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -17,10 +17,16 @@ cd public
 git add -A
 git commit -m 'deploy'
 
+git config --global http.sslVerify false
+git config --global --unset http.proxy
+git config --global --unset https.proxy
+
 # 如果你想要部署到 https://USERNAME.github.io
 # git push -f git@github.com:USERNAME/USERNAME.github.io.git master
 
 # 如果发布到 https://USERNAME.github.io/<REPO>  REPO=github上的项目
-git push -f git@github.com:huaufe/mjxWebBlog.git master:gh-pages
+# git push -f git@github.com:huaufe/mjxWebBlog.git master:gh-pages
+
+git push -f origin HEAD:gh-pages
 
 cd -
